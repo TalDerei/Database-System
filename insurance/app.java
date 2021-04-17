c02f6332461a8bbe7df3d0ef87ae227ea3a34906
Tal Derei <Tad222@lehigh.edu>
2021-04-17 15:45:50 -0700

    added CLI interface structure

--- insurance/app.java
+++ insurance/app.java
@@ -7,32 +7,64 @@
  */
 
 import java.io.*;
-import java.util.*;
-import java.sql.*;
-import java.text.*;;
+import java.util.Scanner;
 
 /**
  * User-interface for insurance database
  */
 public class app {
     public static void main(String[] args) {
-    // input variables
+        /**
+         * User database login credentials -- username and password
+         */
         String user;
         String pass;
-        boolean connection_value = true;
+        boolean connection_status = true;
 
-        while (connection_value) {
-            // Scanner class used to get user input
-            Scanner input = new Scanner(System.in);
-            
-            // prompt username and passwords 
+        /**
+         * Connect to the database
+         */
+        Scanner input = new Scanner(System.in);
+        while (connection_status) {
             System.out.print("enter Oracle user id: ");   
             user = input.nextLine();
             System.out.print("enter Oracle password for " + user + ": ");
             pass = input.nextLine();
 
-            // execute connect_database method
-            connection_value = new insurance().connect_database(user, pass);
+            connection_status = new insurance().connect_database(user, pass);
+        }
+
+        /**
+         * Connect to customer, agent, adjuster, or corperate interfaces
+         */
+        System.out.println("Enter [1] customer\n");
+        System.out.println("Enter [2] agent\n");
+        System.out.println("Enter [3] adjuster\n");
+        System.out.println("Enter [4] corporate management\n");
+        int interfaces;
+        while (true) {
+            boolean condition = input.hasNextInt();
+            if (condition) {
+                interfaces = input.nextInt();
+                if (interfaces == 1) {
+                    new insurance().customer(interfaces);
+                }
+                else if (interfaces == 2) {
+                    new insurance().agent(interfaces);
+                }
+                else if (interfaces == 3) {
+                    new insurance().adjuster(interfaces);
+                }
+                else if (interfaces == 4) {
+                    new insurance().corperate(interfaces);
+                }
+            }
+            else {
+                System.out.println("invalid input! Try again!");
+                input.next();
+            }
         }
     }
 }
+
+
